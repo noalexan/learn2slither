69f3c9e366ee4274c51eac7138e5792e7ba4b38e
Noah Alexandre <noah.alxdr+github@gmail.com>
2026-04-14 19:54:33 +0200

    fix: Add checks for interger overflows

--- src/state.rs
+++ src/state.rs
@@ -14,39 +14,16 @@ impl State {
         let snake = board.snake();
         let head = snake.head();
 
+        let tile_or_wall = |x: Option<usize>, y: Option<usize>| match (x, y) {
+            (Some(x), Some(y)) if x < board.width() && y < board.height() => board.at(x, y),
+            _ => TileType::Wall,
+        };
+
         State {
-            up: core::array::from_fn(|i| {
-                let y = head.y - i;
-                if y < board.height() {
-                    board.at(head.x, y)
-                } else {
-                    TileType::Wall
-                }
-            }),
-            down: core::array::from_fn(|i| {
-                let y = head.y + i;
-                if y < board.height() {
-                    board.at(head.x, y)
-                } else {
-                    TileType::Wall
-                }
-            }),
-            left: core::array::from_fn(|i| {
-                let x = head.x - i;
-                if x < board.width() {
-                    board.at(x, head.y)
-                } else {
-                    TileType::Wall
-                }
-            }),
-            right: core::array::from_fn(|i| {
-                let x = head.x + i;
-                if x < board.width() {
-                    board.at(x, head.y)
-                } else {
-                    TileType::Wall
-                }
-            }),
+            up: core::array::from_fn(|i| tile_or_wall(Some(head.x), head.y.checked_sub(i))),
+            down: core::array::from_fn(|i| tile_or_wall(Some(head.x), head.y.checked_add(i))),
+            left: core::array::from_fn(|i| tile_or_wall(head.x.checked_sub(i), Some(head.y))),
+            right: core::array::from_fn(|i| tile_or_wall(head.x.checked_add(i), Some(head.y))),
         }
     }
 }
